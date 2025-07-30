6217f6495f6d2733ba2f9608da9463466403102f
MD SANAULLAH <msanaullah451@gmail.com>
2025-07-30 11:04:26 +0530

    Update pass_fail.py

--- codes_in_JULY2025/pass_fail.py
+++ codes_in_JULY2025/pass_fail.py
@@ -2,6 +2,8 @@ marks1 = int(input("Enter mark of Maths : "))
 marks2 = int(input("Enter mark of Physics : "))
 marks3 = int(input("Enter mark of Chemistry : "))
 
+#total mark for each subject is 100
+
 total_percentage = 100*(marks1 + marks2 + marks3)/300
 
 if (total_percentage >= 40 and marks1 >= 33 and marks2 >= 33 and marks3 >= 33):
