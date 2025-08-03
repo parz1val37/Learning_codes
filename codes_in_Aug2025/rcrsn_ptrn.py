774d6355fe78912817c6c1474d2e41c6a8134106
MD SANAULLAH <msanaullah451@gmail.com>
2025-08-03 18:34:31 +0530

    Update rcrsn_ptrn.py

--- codes_in_Aug2025/rcrsn_ptrn.py
+++ codes_in_Aug2025/rcrsn_ptrn.py
@@ -1,8 +1,13 @@
 def star_ptrn(n):
     if n == 0:
+        print("No pattern for zero row.")
+        return
+    elif n < 0:
+        print("Negative intezers are not allowed.")
         return
     else:
         print("*" * n)
     star_ptrn(n - 1)
 n = int(input("Enter the number of rows: "))
 star_ptrn(n)
+
