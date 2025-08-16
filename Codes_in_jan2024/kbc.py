7be3195f046630f6c5c70e8fb846df71b7f8b46a
MD SANAULLAH <msanaullah451@gmail.com>
2025-08-16 23:18:48 +0530

    Update kbc.py

--- Codes_in_jan2024/kbc.py
+++ Codes_in_jan2024/kbc.py
@@ -1,30 +1,39 @@
 qns = ["q1: 3 + 5 =","q2: 4 * 5 =","q3: 9 / 3 ="]
 score = 0
-print(qns[0])
-a = int(input("ENTER ANSWER:"))
-match a:
-    case _ if a == 8:
-        print("RIGHT")
-        score += 1
-    case _:
-        print("WRONG")
 
-print(qns[1])
-b = int(input("ENTER ANSWER:"))
-match b:
-    case _ if b == 20:
-        print("RIGHT")
-        score += 1
-    case _:
-        print("WRONG")
-        
-print(qns[2])
-c = int(input("ENTER ANSWER:"))
-match c:
-    case _ if c == 3:
-        print("RIGHT")
-        score += 1
-    case _:
-        print("WRONG")
+try:
+    print(qns[0])
+    a = int(input("ENTER ANSWER:"))
+    match a:
+        case _ if a == 8:
+            print("RIGHT")
+            score += 1
+        case _:
+            print("WRONG")
+except:
+    print("Error")
 
+try:
+    print(qns[1])
+    b = int(input("ENTER ANSWER:"))
+    match b:
+        case _ if b == 20:
+            print("RIGHT")
+            score += 1
+        case _:
+            print("WRONG")
+except:
+    print("Error")   
+
+try:
+    print(qns[2])
+    c = int(input("ENTER ANSWER:"))
+    match c:
+        case _ if c == 3:
+            print("RIGHT")
+            score += 1
+        case _:
+            print("WRONG")
+except:
+    print("Error")
 print(f"your total score is: {score}/3")
