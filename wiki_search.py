9e5162566f29a5682d10670c263d556311679b93
MD SANAULLAH <msanaullah451@gmail.com>
2025-09-05 11:44:03 +0530

    Update wiki_search.py

--- wiki_search.py
+++ wiki_search.py
@@ -9,7 +9,7 @@ try:
     print("\nResult:\n")
     print(summary)
 
-    # Ask if user wants full page
+    # Asks if user wants full page
     choice = input("\nDo you want to open the full Wikipedia page? (yes/no): ").lower()
     if choice == "yes":
         page = wikipedia.page(topic)
@@ -17,6 +17,6 @@ try:
 
 except wikipedia.exceptions.DisambiguationError as e:
     print("Your search was too broad.")
-    # print(e.options)
+    
 except wikipedia.exceptions.PageError:
-    print("No page found for that topic.")
+    print("No page found for this topic.")
