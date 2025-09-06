f0e99a0ab2e57b1eddb70127f41476ec491c86cd
MD SANAULLAH <msanaullah451@gmail.com>
2025-09-06 23:10:32 +0530

    Update Permutation.py
    more clear

--- Permutation.py
+++ Permutation.py
@@ -1,22 +1,16 @@
+import math
 print("Permutations of 'n' distinct objects taken 'r' at a time is given by:\nnPr = n!/(n-r)!, where \"!\" denotes the factorial.\n")
-
 
 try:
     n = int(input("Enter the value of n (n>0): "))
-    if n>0:
-        r = int(input("Enter the value of r (r>=0): "))
-    elif n<=0:
-        print("Enter a valid input")
-        exit()
-
-    def factorial(x):
-        if x==0 or x==1:
-            return 1
-        else:
-            return x*factorial(x-1)
-
-    ans = (factorial(n))/(factorial(n-r))
-    print(f"\nPermutation of {n} distinct objects taken {r} at a time is: {int(ans)}")
+    r = int(input("Enter the value of r (0<=r<=n): "))
+    if n<=0 or r<0:
+        print("'n' should be greater than 0 and 'r' must be >=0")
+    elif r>n:
+        print("'r' can't be greater than 'n'")
+    else:
+        ans = (math.factorial(n))//(math.factorial(n-r))
+        print(f"\nPermutation of {n} distinct objects taken {r} at a time is: {ans}")
 
 except Exception as e:
     print(f"ERROR: {e}")
